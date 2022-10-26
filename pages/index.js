0245aa5be4cbf5184fe35bee10a9eb5818fb3d94
inaciofabricio <inacio.fabricio.nunes@gmail.com>
2022-10-26 18:15:18 -0300

    excluindo a page sobre

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import NextLink from "next/link";
-
 export default function HomeScreen() {
   return (
     <div>
       <h1>Página Inicial - teste 3</h1>
-      <NextLink href="/sobre">
-        <a>Ir para a sobre</a>
-      </NextLink>
     </div>
   );
 }
